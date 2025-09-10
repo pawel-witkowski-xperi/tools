f549a042305b424b96d3c0b010785bc27d4b7ff8
Pawel Witkowski <pawel.witkowski@xperi.com>
2025-09-10 12:28:31 +0200

    Further improvements of downloader.sh
    To recognize url elements params as well as flashing with sideloaded
    archive.

--- downloader/downloader.sh
+++ downloader/downloader.sh
@@ -2,30 +2,77 @@
 
 set -e
 
-if [[ $# -ne 2 ]]; then
-  echo "Usage: downloader.sh DEVICE_IP FIRMWARE_URL" >&2
-  exit 1
-fi
+function show_help() {
+  echo "Tool to download and install or simply install given firmware on a device. Usage: "
+  echo "--device-ip         IP of a device under flash, e.g. --device-ip=192.168.0.1"
+  echo "--firmware-tar     Skip download and simply install the firmware defined by this variable,
+                            e.g. --firmware-tar=/mnt/packages/tivo-os-image-18.847.0-ng-dev-aml-t962d4-cvte-xperi.tar.gz"
+  echo "--firmware-url      URL to be downloaded and installed, if it's specified then
+                            --firmware-tar param is omitted, 
+                            e.g. --firmware-url=https://builds.corp.vewd.com/yocto/tv-master/2025-09-08-18.847.0/xperi/cvte/aml-t962d4-dev/tivo-os-image-18.847.0-ng-dev-aml-t962d4-cvte-xperi.tar.gz"
+  echo "--branch            If --firmware-url is not specified, this is the branch to
+                            be used by the fget downloader, e.g. --branch=tv-master"
+  echo "--build             If --firmware-url is not specified, this is the build to
+                            be used by the fget downloader, e.g. --build=latest, --build=18.848.0"
+  echo "--customer          If --firmware-url is not specified, this is the customer to
+                            be used by the fget downloader, e.g. --customer=xperi"
+  echo "--sdk               If --firmware-url is not specified and there are multiple yocto versions
+                            available for given customer, this switch is used to specify the 
+                            used one, e.g. --sdk=4-0. Allowed values: 3-1 and 4-0. Default 3-1."
+  echo "--motherboard       If --firmware-url is not specified, this is the motherboard variant to
+                            be used by the fget downloader, e.g. --motherboard=cvte"
+  echo "--platform          If --firmware-url is not specified, this is the platform to
+                            be used by the fget downloader, e.g. --platform=aml-t962d4"
+}
 
-DEVICE_IP=$1
-FIRMWARE_URL=$2
+DEVICE_IP=""
+FIRMWARE_URL=""
+FIRMWARE_TAR=""
+BRANCH=""
+BUILD=""
+CUSTOMER=""
+SDK=""
+MOTHERBOARD=""
+PLATFORM=""
 
-PYVENV=".venv"
-ARTIFACTORY="https://repo-vip.tivo.com/artifactory/api/pypi/pypi/simple"
 TEMP_DIR=$(mktemp -d)
 EXEC_PATH="/testing"
 
 SW_UPDATE_PACKAGE_INSTALL="sw-updater-package-install"
 SW_UPDATE_PACKAGE_INSTALL_BIN="sw-updater-package-install.bin"
-SW_UPDATE_PKG="$(basename -s .tar.gz $FIRMWARE_URL).pkg"
-TAR_NAME="$(basename $FIRMWARE_URL)"
+SW_UPDATE_PKG=""
+TAR_NAME="firmware.tar.gz"
 
-if [[ ! -d "$PYVENV"  ]]; then
+for arg do
+  shift
+  case "${arg}" in
+    --help) show_help; exit 0;;
+    --device-ip=*) DEVICE_IP="${arg#--device-ip=}";continue;;
+    --firmware-tar=*) FIRMWARE_TAR="${arg#--firmware-tar=}";continue;;
+    --firmware-url=*) FIRMWARE_URL="${arg#--firmware-url=}";continue;;
+    --branch=*) BRANCH="${arg#--branch=}";continue;;
+    --build=*) BUILD="${arg#--build=}";continue;;
+    --customer=*) CUSTOMER="${arg#--customer=}";continue;;
+    --sdk=*) SDK="${arg#--sdk=}";continue;;
+    --motherboard=*) MOTHERBOARD="${arg#--motherboard=}";continue;;
+    --platform=*) PLATFORM="${arg#--platform=}";continue;;
+  esac
+done
+
+if [[ -z "$DEVICE_IP" ]]; then
+  echo "Please specify the device"
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname $0)
+PYVENV=".venv"
+ARTIFACTORY="https://repo-vip.tivo.com/artifactory/api/pypi/pypi/simple"
+if [[ ! -d "$SCRIPT_DIR/$PYVENV"  ]]; then
   python3 -m venv $PYVENV
-  source $PYVENV/bin/activate
-  pip3 install fget --index=https://repo-vip.tivo.com/artifactory/api/pypi/pypi/simpleo
+  source $SCRIPT_DIR/$PYVENV/bin/activate
+  pip3 install fget --index=$ARTIFACTORY
 else
-  source $PYVENV/bin/activate
+  source $SCRIPT_DIR/$PYVENV/bin/activate
 fi
 
 function cleanup() {
@@ -33,22 +80,45 @@ function cleanup() {
   echo "[rm] $TEMP_DIR"
 }
 
+function print_fw() {
+  echo "[setup] Print device state before flashing"
+  ssh "root@$DEVICE_IP" "cat /etc/os-release"
+  echo "[setup] Done"
+}
+
 function download_package() {
-  echo "[download] Downloading $TAR_NAME"
+  echo "[download] Downloading package..."
   # fget bug: does not allow directory in --destination param
-  python3 -m fget --url $FIRMWARE_URL --destination $TEMP_DIR/$TAR_NAME
+  FIRMWARE_TAR="$TEMP_DIR/$TAR_NAME"
+  if [[ -n "$FIRMWARE_URL" ]]; then
+    python3 -m fget --url "$FIRMWARE_URL" --destination "$FIRMWARE_TAR"
+  else
+    CUSTOMER_ELEMENT=""
+    if [[ -z "$SDK" || "$SDK" -eq "3-1" ]]; then
+      CUSTOMER_ELEMENT="$CUSTOMER"
+    else
+      CUSTOMER_ELEMENT="$CUSTOMER-$SDK"
+    fi
+    BOARD_ELEMENT="$PLATFORM*-dev"
+    ARCHIVE_ELEMENT="$CUSTOMER.tar.gz"
+    python3 -m fget --url_elements yocto "$BRANCH" "$BUILD" "$CUSTOMER_ELEMENT" "$MOTHERBOARD" "$BOARD_ELEMENT" "$ARCHIVE_ELEMENT" --destination "$FIRMWARE_TAR"
+  fi
   echo "[download] Done"
 }
 
 function untar_package() {
-  echo "[untar] Unpacking $TAR_NAME"
-  tar -xf "$TEMP_DIR/$TAR_NAME" -C $TEMP_DIR
+  echo "[untar] Unpacking $FIRMWARE_TAR"
+  tar -xf "$FIRMWARE_TAR" -C "$TEMP_DIR"
+  SW_UPDATE_PKG=$(basename $(find $TEMP_DIR -type f -name "*.pkg"))
   echo "[untar] Done"
 }
 
 function upload_files() {
   echo "[upload] Uploading $SW_UPDATE_PACKAGE_INSTALL $SW_UPDATE_PACKAGE_INSTALL_BIN $SW_UPDATE_PKG"
-  scp "$TEMP_DIR/$SW_UPDATE_PACKAGE_INSTALL" "$TEMP_DIR/$SW_UPDATE_PACKAGE_INSTALL_BIN" "$TEMP_DIR/$SW_UPDATE_PKG" "root@$DEVICE_IP:$EXEC_PATH"
+  scp "$TEMP_DIR/$SW_UPDATE_PACKAGE_INSTALL" "$TEMP_DIR/$SW_UPDATE_PKG" "root@$DEVICE_IP:$EXEC_PATH"
+  if [[ -f "$TEMP_DIR/$SW_UPDATE_PACKAGE_INSTALL_BIN" ]]; then
+    scp "$TEMP_DIR/$SW_UPDATE_PACKAGE_INSTALL_BIN" "root@$DEVICE_IP:$EXEC_PATH"
+  fi
   echo "[upload] Done"
 }
 
@@ -80,12 +150,14 @@ function reboot_device() {
   echo "[reboot] Calling device reboot in 1..."
   sleep 1
   ssh "root@$DEVICE_IP" "/sbin/reboot"
-
 }
 
 trap cleanup EXIT TERM INT
 
-download_package
+print_fw
+if [[ -z "$FIRMWARE_TAR" ]]; then
+  download_package
+fi
 untar_package
 upload_files
 flash_device
